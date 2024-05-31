f4664c56f5a10b58c67df9c7b546e66495aa978d
Juan Urricariet <44036018+jurricariet@users.noreply.github.com>
2024-05-31 17:52:31 -0300

    Add files via upload

---
+++ cash_flow_bonos.R
@@ -0,0 +1,100 @@
+library(tidyverse)
+
+
+bonos <- list()
+ruta <- 'C:/Users/jiurr/Downloads/tp_bonos.xlsx'
+
+hojas <- length( readxl::excel_sheets(ruta ))
+
+lista_bonos <- readxl::read_excel(ruta,sheet=1)
+
+for (i in 2:hojas){
+tabla <- readxl::read_excel(ruta,sheet=i)
+tabla$simbolo = lista_bonos[i-1,'simbolo'] %>% pull(simbolo)
+tabla$ley = lista_bonos[i-1,'ley'] %>% pull(ley)
+bonos[[i-1]] <- tabla
+rm(tabla)
+}
+
+bonos_df <- do.call(rbind,bonos)
+
+
+bonos_fecha <- bonos_df %>% 
+  group_by(fecha,ley) %>% 
+  summarise(intereses = sum(monto_interes),
+            amortizacion = sum(monto_amortizacion))
+
+
+bonos_fecha %>% 
+  pivot_longer(cols=c(intereses,amortizacion),names_to="tipo",values_to = "val") %>% 
+  ggplot(aes(as.Date(fecha),y=val,group=tipo,fill=tipo))+
+  geom_col()+
+  theme_minimal()+
+  ggthemes::scale_fill_economist(name='')+
+  scale_y_continuous(labels = scales::number_format(scale = 1/1e+6,big.mark = '.'))+
+  theme(legend.position = 'top')+
+  scale_x_date(date_breaks='1 year',date_labels = '%Y')+
+  labs(x='',y='',title='Vencimientos de bonos en dólares',
+       subtitle='En millones de USD')
+
+
+bonos_fecha %>% 
+  pivot_longer(cols=c(intereses,amortizacion),names_to="tipo",values_to = "val") %>% 
+  ggplot(aes(as.Date(fecha),y=val,group=tipo,fill=tipo))+
+  geom_col()+
+  theme_minimal()+
+  ggthemes::scale_fill_economist(name='')+
+  scale_y_continuous(labels = scales::number_format(scale = 1/1e+6,big.mark = '.'))+
+  theme(legend.position = 'top')+
+  scale_x_date(date_breaks='3 years',date_labels = '%Y')+
+  facet_wrap(~ley)+
+  labs(x='',y='',title='Vencimientos de bonos en dólares',
+       subtitle='En millones de USD')
+
+
+
+bonos_anio <- bonos_df %>% 
+  mutate(anio = lubridate::year(fecha)) %>% 
+  group_by(anio,ley) %>% 
+  summarise(intereses = sum(monto_interes),
+            amortizacion = sum(monto_amortizacion))
+
+
+bonos_fecha %>% 
+  pivot_longer(cols=c(intereses,amortizacion),names_to="tipo",values_to = "val") %>% 
+  ggplot(aes(as.Date(fecha),y=val,group=tipo,fill=tipo))+
+  geom_col()+
+  theme_minimal()+
+  ggthemes::scale_fill_economist(name='')+
+  scale_y_continuous(labels = scales::number_format(scale = 1/1e+6,big.mark = '.'))+
+  theme(legend.position = 'top')+
+  scale_x_date(date_breaks='1 year',date_labels = '%Y')+
+  labs(x='',y='',title='Vencimientos de bonos en dólares',
+       subtitle='En millones de USD')
+
+
+bonos_anio %>% 
+  pivot_longer(cols=c(intereses,amortizacion),names_to="tipo",values_to = "val") %>% 
+  ggplot(aes(as.factor(anio),y=val,group=tipo,fill=tipo))+
+  geom_col()+
+  theme_minimal()+
+  ggthemes::scale_fill_economist(name='')+
+  scale_y_continuous(labels = scales::number_format(scale = 1/1e+6,big.mark = '.'))+
+  theme(legend.position = 'top',
+        axis.text.x = element_text(angle=90))+
+  facet_wrap(~ley)+
+  labs(x='',y='',title='Vencimientos de bonos en dólares',
+       subtitle='En millones de USD. Por jurisdicción.')
+
+bonos_anio %>% 
+  pivot_longer(cols=c(intereses,amortizacion),names_to="tipo",values_to = "val") %>% 
+  ggplot(aes(as.factor(anio),y=val,group=tipo,fill=tipo))+
+  geom_col()+
+  theme_minimal()+
+  ggthemes::scale_fill_economist(name='')+
+  scale_y_continuous(labels = scales::number_format(scale = 1/1e+6,big.mark = '.'))+
+  theme(legend.position = 'top',
+        axis.text.x = element_text(angle=90))+
+  labs(x='',y='',title='Vencimientos de bonos en dólares',
+       subtitle='En millones de USD')
+
